46fcb1a602cfce1f2e5ff4e6f415cd65238b3595
Bo Simonsen <bo@geekworld.dk>
2014-01-15 19:35:14 +0100

    pep8

--- collective/fof/interfaces.py
+++ collective/fof/interfaces.py
@@ -4,5 +4,6 @@ from zope.interface import Interface
 class IBrowserLayer(Interface):
     """ Browser layer marker interface """
 
+
 class IFourOhFourPage(Interface):
     """ Marker interface for the 404 page """
